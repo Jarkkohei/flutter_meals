761c4d596e1c06ed4e393f5daf3690c5e6f10e0f
Jarkko <jarkkohei@luukku.com>
2019-07-09 16:20:42 +0300

    Add a fontWeight to the theme title

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
               title: TextStyle(
                 fontSize: 20,
                 fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
               ),
             ),
       ),
