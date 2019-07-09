b8eaf428fb4d5d6118d41044d7b77bd5afd002a2
Jarkko <jarkkohei@luukku.com>
2019-07-09 11:48:06 +0300

    Convert the MyHomePage-widget from Stateless- to StatefulWidget and change the AppBar title

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,19 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
 
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Flutter Meals App'),
       ),
       body: Center(
         child: Text('Widget playground'),
