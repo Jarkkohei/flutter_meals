63ed77a1b39175c51ac2589c932a9b65dff13d16
Jarkko <jarkkohei@luukku.com>
2019-07-17 17:25:59 +0300

    Register the MealDetailScreen as a route with the static routeName

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meal-detail';
+  
   @override
   Widget build(BuildContext context) {
     return Center(
