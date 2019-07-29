ab83f9d54283004ee39e693031004d0eed788936
Jarkko <jarkkohei@luukku.com>
2019-07-29 13:22:09 +0300

    Wrap the ListView showing the ingredients with the buildContainer

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -51,17 +51,8 @@ class MealDetailScreen extends StatelessWidget {
               ),
             ),
             buildSectionTtitle(context, 'Ingredients'),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.all(10),
-              height: 150,
-              width: 300,
-              child: ListView.builder(
+            buildContainer(
+              ListView.builder(
                 itemCount: selectedMeal.ingredients.length,
                 itemBuilder: (ctx, index) => Card(
                   color: Theme.of(context).accentColor,
