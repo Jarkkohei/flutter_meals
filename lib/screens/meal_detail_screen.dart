0f7c063f85c3e196f5fd65bf4b5bc8dedacac6ad
Jarkko <jarkkohei@luukku.com>
2019-07-29 13:16:13 +0300

    Convert the title -conteiner to builder-function and use it also to render the steps-conteiner

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -5,6 +5,16 @@ import '../dummy_data.dart';
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
 
+  Widget buildSectionTtitle(BuildContext context, String text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.title,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
@@ -25,13 +35,7 @@ class MealDetailScreen extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                'Ingredients',
-                style: Theme.of(context).textTheme.title,
-              ),
-            ),
+            buildSectionTtitle(context, 'Ingredients'),
             Container(
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -54,6 +58,7 @@ class MealDetailScreen extends StatelessWidget {
                 ),
               ),
             ),
+            buildSectionTtitle(context, 'Steps'),
           ],
         ),
       ),
